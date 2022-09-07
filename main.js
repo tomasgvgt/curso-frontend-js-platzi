1009e005fce20931661f8024e478b55de9c4f0bc
tomasgvgt <tomasgvg.t@gmail.com>
2022-09-07 18:39:41 -0500

    The product details component is now personalized for each product and its fully functional

--- main.js
+++ main.js
@@ -4,42 +4,57 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu')
 const mobileHamburguerMenuIcon = document.querySelector('.mobile-hamburguer-menu');
 const myOrders = document.querySelector(".my-orders");
-const productDetails = document.querySelector(".product-detail");
-const prodDetailsCloseIcon = document.querySelector(".product-detail-close");
+let productDetails;
+let prodDetailsCloseIcon;
 const cardsContainer = document.querySelector(".cards-container");
 
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 mobileHamburguerMenuIcon.addEventListener('click', toggleMobileMenu);
 shoppingCart.addEventListener('click', toggleMyOrders);
-prodDetailsCloseIcon.addEventListener('click', closeProdDetails);
 
 
 
 function toggleDesktopMenu(){
+    if(productDetails != undefined){
+        productDetails.classList.add('inactive');
+    }
     myOrders.classList.add('inactive');
     productDetails.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    if(productDetails != undefined){
+        productDetails.classList.add('inactive');
+    }
     myOrders.classList.add('inactive');
     productDetails.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
 }
 
 function toggleMyOrders(){
+    if(productDetails != undefined){
+        productDetails.classList.add('inactive');
+    }
     desktopMenu.classList.add('inactive');
     mobileMenu.classList.add('inactive');
     productDetails.classList.add('inactive');
     myOrders.classList.toggle('inactive');
 }
 
-function openProdDetails(){
+
+function openProdDetails(event){
+    if(productDetails != undefined){
+        productDetails.classList.add('inactive');
+    }
+    const id = event.target.parentElement.id;
+    const prodDetails = document.querySelector(`.${id}`);
     desktopMenu.classList.add('inactive');
     mobileMenu.classList.add('inactive');
     myOrders.classList.add('inactive');
-    productDetails.classList.remove('inactive');
+    prodDetails.classList.remove('inactive');
+    productDetails = prodDetails;
 }
 
 function closeProdDetails(){
@@ -47,24 +62,43 @@ function closeProdDetails(){
 }
 
 
-function product(prodName, price, imgUrl){
+
+function product(_id, prodName, price, imgUrl, description){
+    this._id = _id;
     this.prodName = prodName;
     this.price = price;
     this.imgUrl = imgUrl;
+    this.description = description;
 }
-
 
 //List of articles being sold in the ecommerce.
 const productList = [];
-productList.push(new product("Glasses", 250, "https://image4.cdnsbg.com/2/73/601369_1651585367163.jpg?width=900&height=450&q=90"));
-productList.push(new product("sneaker", 100, "https://media1.popsugar-assets.com/files/thumbor/_pb-tGgTHMuKTwztuYu2Gu0P2Aw/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/11/07/864/n/1922564/4bd3f98bbff821a9_netimgcNQjT8/i/Vans-Style-36.jpg"));
-productList.push(new product("tshirt", 50, 'https://img01.ztat.net/article/spp-media-p1/8f6d6138e437492b8893c5b3b0aa650f/ad5fd4218dc34c2f90f77818950775a5.jpg?imwidth=762'));
+productList.push(new product(
+    'g1',
+    "Glasses",
+    250,
+    "https://image4.cdnsbg.com/2/73/601369_1651585367163.jpg?width=900&height=450&q=90",
+    "A pair of nice rowndly shaped honey colored glasses."));
+productList.push(new product(
+    's1',
+    "Sneakers",
+    100,
+    "https://media1.popsugar-assets.com/files/thumbor/_pb-tGgTHMuKTwztuYu2Gu0P2Aw/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/11/07/864/n/1922564/4bd3f98bbff821a9_netimgcNQjT8/i/Vans-Style-36.jpg",
+    "A pair of orange vans sneakers that strike the eye."));
+productList.push(new product(
+    't1',
+    "tshirt",
+    50,
+    'https://img01.ztat.net/article/spp-media-p1/8f6d6138e437492b8893c5b3b0aa650f/ad5fd4218dc34c2f90f77818950775a5.jpg?imwidth=762',
+    "Comfortable fitted black tshirt."));
 
 //articles and their container HTML
-function renderProducts(arr){
+function renderProductsAndInfo(arr){
     for(product of arr){
+        //render product details
         const prodCard = document.createElement('div');
-        prodCard.classList.add('product-card');
+        prodCard.classList.add(`product-card`);
+        prodCard.setAttribute('id', product._id);
         cardsContainer.appendChild(prodCard);
 
         const prodImg = document.createElement('img');
@@ -92,8 +126,52 @@ function renderProducts(arr){
         addToCartImg.setAttribute('src', "./icons/bt_add_to_cart.svg");
         addToCart.appendChild(addToCartImg);
         prodInfo.appendChild(addToCart);
+
+        //Render product detail cards
+        const prodDetailsCard = document.createElement('aside');
+        prodDetailsCard.classList.add(`product-detail`);
+        prodDetailsCard.classList.add('inactive');
+        prodDetailsCard.classList.add(product._id);
+        document.body.appendChild(prodDetailsCard);
+
+        prodDetailsCloseIcon = document.createElement('div');
+        prodDetailsCloseIcon.classList.add('product-detail-close');
+        prodDetailsCard.appendChild(prodDetailsCloseIcon);
+        prodDetailsCloseIcon.addEventListener('click', closeProdDetails);
+
+        const prodDetailsCloseImg = document.createElement('img');
+        prodDetailsCloseImg.setAttribute('src', './icons/icon_close.png');
+        prodDetailsCloseIcon.appendChild(prodDetailsCloseImg);
+
+        const prodImg2 = document.createElement('img');
+        prodImg2.setAttribute('src', product.imgUrl);
+        prodDetailsCard.appendChild(prodImg2);
+
+        const prodInfo2 = document.createElement('div');
+        prodInfo2.classList.add('product-info');
+        prodDetailsCard.appendChild(prodInfo2);
+
+        const prodPrice2 = document.createElement('p');
+        prodPrice2.innerHTML = '$' + product.price;
+        prodInfo2.appendChild(prodPrice2);
+
+        const prodName2 = document.createElement('p');
+        prodName2.innerHTML =  product.prodName;
+        prodInfo2.appendChild(prodName2);
+
+        const prodDescription= document.createElement('p');
+        prodDescription.innerHTML =  product.description;
+        prodInfo2.appendChild(prodDescription);
+
+        const addToCartButton = document.createElement('button');
+        addToCartButton.classList.add('primary-button');
+        addToCartButton.classList.add('add-to-cart-button');
+        addToCartButton.innerHTML = 'Add to cart';
+        prodInfo2.appendChild(addToCartButton);
+
+        const  addToCartButtonImg = document.createElement('img');
+        addToCartButtonImg.setAttribute('src', './icons/bt_add_to_cart.svg');
+        addToCartButton.appendChild(addToCartButtonImg);
     }
 }
-renderProducts(productList);
-
-
+renderProductsAndInfo(productList);
